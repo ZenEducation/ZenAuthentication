6a12b15f0b242161022bafd474d36e09db55bf02
kishan kantariya <kishankantariya111@gmail.com>
2023-07-01 23:08:12 +0530

    intial commit

---
+++ pages/register.vue
@@ -0,0 +1,11 @@
+<template>
+  <RegisterForm v-if="!userEmail" @setEmail = "setEmail"/>
+  <RegisterOtpForm v-else :email="userEmail" @setEmail = "setEmail"/>
+</template>
+
+<script setup>
+const userEmail = ref('')
+const setEmail = () => {
+  userEmail.value = email
+}
+</script>
