70d813f882f1f9ea7ab8f2b0dd933654183f5fd5
kishan kantariya <kishankantariya111@gmail.com>
2023-07-04 11:29:14 +0530

    added amplify files

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,7 @@
               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
           </div>
         </div>
+        <p class="text-[red]" v-if="errorMsg.status">{{ errorMsg.message }}</p>
 
         <div>
           <button type="submit" class="bg-indigo-600 border px-6 py-2 text-white" @click.prevent="onSubmit">Sign
